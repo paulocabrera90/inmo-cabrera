d210e4355be5af3fb7a1ebf06de5d34535da2cb7
Paulo Cabrera <nicolas.cabrera@redb.ee>
2024-10-14 04:10:00 -0300

    Agrego validacion con JWT y bycrypt

--- Controllers/PropietariosController.cs
+++ Controllers/PropietariosController.cs
@@ -4,19 +4,20 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Swashbuckle.AspNetCore.Annotations;
 
+
 namespace Inmueble_cabrera.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class PropietariosController : ControllerBase
 {
-	private readonly DataContext contexto;
+	private readonly DataContext context;
 	private readonly IConfiguration config;
 	private readonly IWebHostEnvironment environment;
 
-	public PropietariosController(DataContext contexto, IConfiguration config, IWebHostEnvironment env)
+	public PropietariosController(DataContext context, IConfiguration config, IWebHostEnvironment env)
 	{
-		this.contexto = contexto;
+		this.context = context;
 		this.config = config;
 		environment = env;
 	}
@@ -25,7 +26,7 @@ public class PropietariosController : ControllerBase
 	//[SwaggerOperation(Summary = "Retrieves the current weather forecast")]
 	public async Task<IActionResult> GetPropietarios()
 	{
-		var propietarios = await contexto.Propietarios.ToListAsync();
+		var propietarios = await context.Propietarios.ToListAsync();
 		return Ok(propietarios);
 	}
 
@@ -33,7 +34,7 @@ public class PropietariosController : ControllerBase
 	//[SwaggerOperation(Summary = "Retrieves the current weather forecast")]
 	public async Task<IActionResult> GetPropietario(int id)
 	{
-		var propietario = await contexto.Propietarios.FirstOrDefaultAsync(p => p.Id == id);
+		var propietario = await context.Propietarios.FirstOrDefaultAsync(p => p.Id == id);
 
 		if (propietario == null)
 		{
@@ -52,8 +53,8 @@ public class PropietariosController : ControllerBase
 			return BadRequest(ModelState);
 		}
 
-		contexto.Propietarios.Add(propietario);
-		await contexto.SaveChangesAsync();
+		context.Propietarios.Add(propietario);
+		await context.SaveChangesAsync();
 
 		return CreatedAtAction(nameof(GetPropietario), new { id = propietario.Id }, propietario);
 	}
@@ -67,16 +68,16 @@ public class PropietariosController : ControllerBase
 			return BadRequest("ID de propietario no coincide");
 		}
 
-		if (!contexto.Propietarios.Any(e => e.Id == id))
+		if (!context.Propietarios.Any(e => e.Id == id))
 		{
 			return NotFound();
 		}
 
-		contexto.Entry(propietario).State = EntityState.Modified;
+		context.Entry(propietario).State = EntityState.Modified;
 
 		try
 		{
-			await contexto.SaveChangesAsync();
+			await context.SaveChangesAsync();
 		}
 		catch (DbUpdateConcurrencyException)
 		{
@@ -93,25 +94,27 @@ public class PropietariosController : ControllerBase
 		return NoContent();
 	}
 
+/// <response code="201">Returns the newly created item</response>
+/// <response code="400">If the item is null</response>
 	[HttpDelete("{id}")]
 	//[SwaggerOperation(Summary = "Elimina un propietario existente")]
 	public async Task<IActionResult> DeletePropietario(int id)
 	{
-		var propietario = await contexto.Propietarios.FindAsync(id);
+		var propietario = await context.Propietarios.FindAsync(id);
 		if (propietario == null)
 		{
 			return NotFound();
 		}
 
-		contexto.Propietarios.Remove(propietario);
-		await contexto.SaveChangesAsync();
+		context.Propietarios.Remove(propietario);
+		await context.SaveChangesAsync();
 
 		return NoContent();
 	}
 
 	private bool PropietarioExists(int id)
 	{
-		return contexto.Propietarios.Any(e => e.Id == id);
+		return context.Propietarios.Any(e => e.Id == id);
 	}
 
 }
---
+++ Services/TokenService.cs
@@ -0,0 +1,40 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Inmueble_cabrera.Data;
+
+namespace InmobiliariaCA.Services;
+public class TokenService
+{
+    private DataContext context;
+    private IConfiguration config;
+    private IWebHostEnvironment environment;
+
+    public TokenService(DataContext context, IConfiguration config, IWebHostEnvironment environment)
+    {
+        this.context = context;
+        this.config = config;
+        this.environment = environment;
+    }
+
+    public string GenerateJwtToken(string email)
+    {
+        var claims = new[]
+        {
+        new Claim(JwtRegisteredClaimNames.Sub, email),
+        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+    };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            expires: DateTime.Now.AddHours(1),
+            claims: claims,
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
